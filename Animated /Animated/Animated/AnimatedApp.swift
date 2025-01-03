8a07e49f32295d11dccbf931bf0c8cc118df753a
Jesus Chapi <jesersu>
2025-01-02 21:32:19 -0500

    add Animated

---
+++ Animated /Animated/Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Jesús Ervin Chapi Suyo on 2/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
